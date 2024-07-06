cd7a10f0bc6563c870bddb6a7a56a2302e051863
Luiz Rosa <luiz.alberto.laus.rosa.com>
2024-07-06 18:27:52 -0300

    env port

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import app from "./app.js";
 
-app.listen(3001, () => console.log('server running in port 3001'))
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log('server running in port 3001'))
